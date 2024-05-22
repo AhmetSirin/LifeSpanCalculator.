174e9ef3ec23b6f87c2f090b5af683f68b719368
Ahmet Şirin <ahmet.sirin.34@outlook.com>
2024-05-22 21:39:40 +0300

    saat ve dakika eklendi

--- kaç saniye yaşadığımızı öğrenelim.py
+++ kaç saniye yaşadığımızı öğrenelim.py
@@ -1,5 +1,5 @@
 from datetime import *
-dogum_tarihi = datetime.strptime(input("Lütfen doğum tarihinizi gün.ay.yıl şeklinde giriniz: "),"%d.%m.%Y")
+dogum_tarihi = datetime.strptime(input("Lütfen doğum tarihinizi gün.ay.yıl saat.dakika şeklinde giriniz: "),"%d.%m.%Y %H.%M")
 yas = datetime.now() - dogum_tarihi
 if yas.total_seconds() > 0:
     print("Siz {} yılından günümüze kadar {} saniye yaşadınız.".format(dogum_tarihi,yas.total_seconds()))
